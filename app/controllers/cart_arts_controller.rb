10474da44c73937b0d0eb950c0b67266bf0c3faa
daihuku99 <dai9huku@gmail.com>
2020-05-09 23:55:08 +0900

    updateing cart_arts

--- app/controllers/cart_arts_controller.rb
+++ app/controllers/cart_arts_controller.rb
@@ -1,2 +1,21 @@
 class CartArtsController < ApplicationController
+  def create
+    art = Art.find(params[:art_id])
+    cart_art = current_user.cart_arts.new
+    cart_art_art = art.id
+    p cart_art.errors
+    cart_art.save
+    redirect_to new_exhibition_path
+  end
+
+  def destroy
+  end
+
+  def empty
+  end
+
+  # private
+  # def cart_art_params
+  #   params.require(:cart_art).permit(:art_id)
+  # end
 end
